0927ae94195af065bab5eb604512501b598a5aa9
LALBJ <905976755@qq.com>
2019-09-06 09:39:47 +0800

    添加了调试界面的接口

---
+++ project/src/main/java/org/csu/ashirt/control/RedirectTestController.java
@@ -0,0 +1,24 @@
+package org.csu.ashirt.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RedirectTestController {
+    @GetMapping("/index")
+    public String viewIndex(){
+        return "index";
+    }
+    @GetMapping("/design")
+    public String viewDesign(){
+        return "design";
+    }
+    @GetMapping("/userInfo")
+    public String viewUserInfo(){
+        return "user-info";
+    }
+    @GetMapping("/checkout")
+    public String viewCheckout(){
+        return "checkout";
+    }
+}
